48b0e7581ae4266a1a1c4a92e23b50105c68a748
mandyxd <waisumpo@gmail.com>
2020-10-07 23:21:20 +0800

    dealer.java first draft

--- Jack/Dealer.java
+++ Jack/Dealer.java
@@ -1,20 +1,30 @@
-package cs3343jk;
-
+package groupProj;
 public class Dealer {
-
-	public static void CreateDeck() {
-		// TODO Auto-generated method stub
+	Dealer dealer;
+	
+	public Dealer() {
+		dealer = new Deck;
+	}
+	
+	public void Dealer::CreateDeck() {
+		dealer.CreateDeck();
+	}
+	
+	public void shuffle(){
+		srand((unsigned int)time(NULL));
+			for (int i = 0; i < 51; i++) {
+				int r = i + (rand() % (52 - i));
+				Card^ temp = dealer->getCard(i);
+				dealer->setCard(dealer->getCard(r), i);
+				dealer->setCard(temp, r);
+			}
+	}
+	
+	public void deal(Hand hand) {
+		hand->add(dealer->getCard(cardNo));
+			cardNo++;
+		if(cardNo == 51)
+			cardNo = 0;
 		
 	}
-
-	public static void shuffle() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public static void deal(Hand hand) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
